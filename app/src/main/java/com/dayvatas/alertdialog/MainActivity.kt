b8d9bd88443a85c3045b033322b1c87f787280a1
Damla Ayvatas <damla.ayvatas@gmail.com>
2023-03-20 01:08:04 +0300

    Alert Dialogs

--- app/src/main/java/com/dayvatas/alertdialog/MainActivity.kt
+++ app/src/main/java/com/dayvatas/alertdialog/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.dayvatas.alertdialog.databinding.ActivityMainBinding
 
 
@@ -19,6 +20,15 @@ class MainActivity : AppCompatActivity() {
     }
     
     fun save (view : View){
-        Toast.makeText(this, "Saved!", Toast.LENGTH_LONG).show()
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle("Save")
+        alert.setMessage("Are you sure?")
+        alert.setPositiveButton("Yes"){dialog, which ->
+            Toast.makeText(applicationContext, "As you wish", Toast.LENGTH_LONG).show()
+        }
+        alert.setNegativeButton("No"){dialog, which ->
+            Toast.makeText(applicationContext, "Not Saved", Toast.LENGTH_LONG).show()
+        }
+        alert.show()
     }
 }
